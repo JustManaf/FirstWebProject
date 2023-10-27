81ca0e77bd20a7af17391a58ad7f9bcadfa6903d
JustManaf <manafalbaqshi@gmail.com>
2023-10-27 23:16:26 +0300

    css_4-exercise_5

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["CSS/Excercises/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'primary-dark': '#040D12',
+        'text-dark': '#93B1A6',
+        'secondary-dark': '#183D3D',
+      },
+    },
+  },
+  plugins: [],
+}
+
